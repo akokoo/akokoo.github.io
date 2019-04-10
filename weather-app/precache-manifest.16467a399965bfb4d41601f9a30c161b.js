6ac4650537f93476cd804d173c455ed55489fc90
Andrey <35377631+akokoo@users.noreply.github.com>
2019-04-11 00:09:34 +0300

    add weather-app

---
+++ weather-app/precache-manifest.16467a399965bfb4d41601f9a30c161b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c60201fe7758884de75d22f0e559f957",
+    "url": "/static/media/weatherIcons.c60201fe.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e8692d2342586365f55",
+    "url": "/static/js/main.8e8692d2.chunk.js"
+  },
+  {
+    "revision": "3654b94a315bd2c1737d",
+    "url": "/static/js/1.3654b94a.chunk.js"
+  },
+  {
+    "revision": "8e8692d2342586365f55",
+    "url": "/static/css/main.ed3a8f9b.chunk.css"
+  },
+  {
+    "revision": "3654b94a315bd2c1737d",
+    "url": "/static/css/1.22046593.chunk.css"
+  },
+  {
+    "revision": "6d3d13eefa177162a527fe4e68aceac8",
+    "url": "/index.html"
+  }
+];
