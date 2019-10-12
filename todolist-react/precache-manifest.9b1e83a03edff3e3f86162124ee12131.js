01b5ee5959bbb1473416732066d8a64acca80658
Andrey <35377631+akokoo@users.noreply.github.com>
2019-10-12 23:01:42 +0300

    add todolist

---
+++ todolist-react/precache-manifest.9b1e83a03edff3e3f86162124ee12131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc673863cc8ef541aa772ce7c458cd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1f0e2823c76752bdc7e",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d800b63e7bf4efde9f03",
+    "url": "/static/css/main.ad7eff55.chunk.css"
+  },
+  {
+    "revision": "a1f0e2823c76752bdc7e",
+    "url": "/static/js/2.a94fbc85.chunk.js"
+  },
+  {
+    "revision": "d800b63e7bf4efde9f03",
+    "url": "/static/js/main.330e2331.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
